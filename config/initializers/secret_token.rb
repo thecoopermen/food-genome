dfa4ef2ce0112f13c75894714d7667712d92c369
jordan <jordancooperman@gmail.com>
2013-05-23 22:15:26 -0400

    create new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodGenome::Application.config.secret_token = '74f8e768c5a2f2085f21eff0a8f8bd83ebdd20c78d85aaf5c49fa3c92da1ae234cb0704b2a84d86d306e5752ab2b9f4740d417c26794bf388dd16b0ed2fafbd9'
