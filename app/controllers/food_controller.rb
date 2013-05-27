101f9937e13d9802ac0600947d8e6a83b37131e9
Ross Cooperman <cooperman@gmail.com>
2013-05-26 22:16:54 -0400

    Skeletal routes, controller, and views for food

---
+++ app/controllers/food_controller.rb
@@ -0,0 +1,19 @@
+class FoodController < ApplicationController
+
+  def index
+    @food = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(params[:food])
+    if @food.save
+      redirect_to foods_path, notice: "#{@food.name} successfully created"
+    else
+      render: new
+    end
+  end
+end
