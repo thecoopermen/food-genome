9a0f0bc128e40bdbce97e9ef93985357eb5906bf
Ross Cooperman <cooperman@gmail.com>
2013-05-26 22:50:58 -0400

    Added a virtual attribute to set traits on a food

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,11 @@ class Food < ActiveRecord::Base
   has_many :genes
   has_many :traits, through: :genes
 
-  attr_accessible :name
+  attr_accessible :name, :traits
 
   validates :name, presence: true
+
+  def traits=(traits)
+    @traits = traits
+  end
 end
