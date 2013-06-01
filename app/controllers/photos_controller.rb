05b926cf4ace5d6731d54d7dad440fb1d2317da9
Ross Cooperman <cooperman@gmail.com>
2013-06-01 02:14:00 -0400

    Restrict photo search to compatible licenses
    Anything that is free for commercial use (with or without attribution
    required) is fine.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
   respond_to :json
 
   def index
-    photos = flickr.photos.search(text: params[:q]).map do |photo|
+    photos = flickr.photos.search(text: params[:q], license: '4,6,7').map do |photo|
       "http://farm#{photo["farm"]}.staticflickr.com/#{photo["server"]}/#{photo["id"]}_#{photo["secret"]}_z.jpg"
     end
     respond_with photos
